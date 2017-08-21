5e2280477a7cd692d0366f5e9131851e48e5f4ae
Paul Hirch <Paul-Jasper@hotmail.de>
2017-08-21 14:22:18 +0200

    Update

--- src/main.js
+++ src/main.js
@@ -9,10 +9,13 @@ let angle = 0;
 let ctx;
 let treeTexture;
 let groundTexture;
-
-let worldWidth = 64;
-let worldHeight = 64;
-
+let now = 0;
+let startTime = 0;
+let useTime = 0;
+let bindTime = 0;
+let renderTime = 0;
+let worldWidth = 16;
+let worldHeight = 16;
 let waterCoustics=0;
 
 let typMap = new Uint16Array(worldWidth*worldHeight);
@@ -63,7 +66,8 @@ function main(){
   render();
 }
 function loadImages(){
-  nullTexture = gl2D.textureFromPixelArray(new Uint8Array([1,1,1]),1,1);
+  nullTexture = gl2D.textureFromPixelArray(new Uint8Array([255,255,255]),1,1);
+  testTexture = gl2D.textureFromFile("./assets/test.png");
   testTexture = gl2D.textureFromFile("./assets/test.png");
   testTexture2 = gl2D.textureFromFile("./assets/test2.png");
   treeTexture = gl2D.textureFromFile("./assets/tree.png");
@@ -161,11 +165,7 @@ function render5(){
   tmpdate = Date.now();
   tmplast = tmpdate;
   gl2D.startScene();
-  tmpdate = Date.now();
-  tmpnow = tmpdate - tmplast;
-  console.log("start time = "+tmpnow);
-  tmpdate = Date.now();
-  tmplast = tmpdate;
+
   for (let ix = 0;ix < width/64;ix++){
     for (let iy = 0;iy < height/32;iy++){
 
@@ -175,21 +175,8 @@ function render5(){
   //gl2D.addQuadImage(testTexture,[0,0,64,32],[64,128, 128,100, 128,64, 64,64],[0,255,0,255]);
   angle++;
   gl2D.matrix.reset();
-  tmpdate = Date.now();
-  tmpnow = tmpdate - tmplast;
-  console.log("add time = "+tmpnow);
-  tmpdate = Date.now();
-  tmplast = tmpdate;
   gl2D.endScene();
-  tmpdate = Date.now();
-  tmpnow = tmpdate - tmplast;
-  console.log("bind time = "+tmpnow);
-  tmpdate = Date.now();
-  tmplast = tmpdate;
   gl2D.renderScene();
-  tmpdate = Date.now();
-  tmpnow = tmpdate - tmplast;
-  console.log("render time = "+tmpnow);
 
   date = Date.now();
   let now = date - last;
@@ -202,54 +189,53 @@ function render() {
 
   let date = Date.now();
   let last = date;
+  let tmplast
 
   waterCoustics++;
   if (waterCoustics > 100) waterCoustics=0;
+
+  tmplast = Date.now();
   gl2D.startScene();
+  date = Date.now();
+  startTime = (startTime*3 + (date - tmplast))/4;
 
-  let Indentation = 0;
-  let posx = 0;
-  let posy = 0;
-  let addPosX = (width / 64 + 2)|0 ;
-  let addPosY = 1;
-  let aktPosX = ((mapPosX|0) + addPosX + addPosY);
-  let aktPosY = ((mapPosY|0) + addPosX - addPosY);
-  for (let iy = -1; iy <= (height/16)|0; iy++) {
-    if (Indentation == 0) {Indentation=1;posy++;posx--;}
-    else Indentation = 0;
-    aktPosX = ((mapPosX|0)+addPosX+addPosY+posx+Indentation);
-    aktPosY = ((mapPosY|0)+addPosX-addPosY+posy);
-    for (let ix = (width / 64)|0; ix >= -1;ix--){
-      aktPosX--;
-      aktPosY--;
-      if (aktPosX < worldWidth && aktPosX >= 0 && aktPosY < worldHeight && aktPosY >= 0){
-        let drawPosX = 64*ix+32*Indentation;
-        let drawPosY = 16*iy;
+  tmplast = Date.now();
 
-        let worldPos = aktPosX+aktPosY*(worldWidth+1);
-        drawGround(worldPos,[drawPosX,drawPosY]);
-
-    //  gl2D.addImage(testTexture,[0,0,64,64],[drawPosX,drawPosY,64,32],[255,255,255,255]);
-
-        //drawTree([drawPosX+32,drawPosY+16]);
-        // context.drawImage(
-        // staticEntity.sprite[version][envcode].texture,
-        // 0, 0,
-        // 64, 64,
-        // drawPosX*cscale, ((drawPosY-overDraw))*cscale|0,
-        // 64*cscale, ((32+overDraw)*cscale)|0
-        // );
-
-      }
+  for (let ix = 0;ix<100;ix++){
+    for (let iy = 0;iy<50;iy++){
+      gl2D.matrix.setTranslate([ix*20,iy*20])
+      gl2D.matrix.addRotate(0.1)
+      gl2D.drawImage(treeTexture,[0,0,32,64],[-16,-64,32,64],[255,255,255,255]);
     }
   }
-  gl2D.endScene();
-  gl2D.renderScene();
+
+  gl2D.matrix.reset();
+
+  gl2D.drawImage(nullTexture,[0,0,1,1],[16,16,256,32],[100,100,100,255]);
+  if ((now/16.66) < 1) gl2D.drawImage(nullTexture,[0,0,1,1],[16,19,(now/16.66)*256,26],[(now/16.66)*255,200,0,255]);
+  else gl2D.drawImage(nullTexture,[0,0,1,1],[16,19,256,26],[200,0,0,255]);
+
+  let fullTime = useTime + bindTime + renderTime;
+  gl2D.drawImage(nullTexture,[0,0,1,1],[16,64,256,32],[100,100,100,255]);
+  gl2D.drawImage(nullTexture,[0,0,1,1],[16,67,(useTime/fullTime)*256,26],[200,0,0,255]);
+  gl2D.drawImage(nullTexture,[0,0,1,1],[16+(useTime/fullTime)*256,67,(bindTime/fullTime)*256,26],[0,200,0,255]);
+  gl2D.drawImage(nullTexture,[0,0,1,1],[16+(useTime/fullTime)*256+(bindTime/fullTime)*256,67,(renderTime/fullTime)*256,26],[0,0,200,255]);
 
   date = Date.now();
-  let now = date - last;
-  console.log("total time = "+now);
-  console.log("--size()---------------------------------------");
+  useTime = (useTime*3 + (date - tmplast))/4;
+
+  tmplast = Date.now();
+  gl2D.endScene();//--
+  date = Date.now();
+  bindTime = (bindTime*3 + (date - tmplast))/4;
+
+  tmplast = Date.now();
+  gl2D.renderScene();//--
+  date = Date.now();
+  renderTime = (renderTime*3 + (date - tmplast))/4;
+
+  date = Date.now();
+  now = (now*3 + (date - last))/4;
 
   setTimeout(render, 10);
 }
